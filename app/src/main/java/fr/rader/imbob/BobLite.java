fcb23b8381e25bbf5096f7bbf0f94b462a1e5219
Rader <raderpy@protonmail.com>
2022-11-01 00:04:57 +0100

    removed unused commented code

--- app/src/main/java/fr/rader/imbob/BobLite.java
+++ app/src/main/java/fr/rader/imbob/BobLite.java
@@ -13,34 +13,13 @@ public class BobLite extends Application {
     private static final int WINDOW_WIDTH = 535;
     private static final int WINDOW_HEIGHT = 375;
 
-//    private final Logger logger;
-    //private final List<AbstractWindow> windows;
-
-    //private final LicensesWindow licensesWindow;
-
     private boolean isWindowResized = false;
 
     private final WindowManager windowManager;
 
     BobLite() {
-//        this.logger = new Logger();
-        //this.windows = new ArrayList<>();
         this.windowManager = new WindowManager();
         this.windowManager.initializeAllWindows();
-
-        /*ProgressBarWindow progressBar = new ProgressBarWindow();
-        TaskListWindow taskListWindow = new TaskListWindow();
-        TaskWindow taskWindow = new TaskWindow();
-        this.licensesWindow = new LicensesWindow();
-
-        taskWindow.setTaskListWindowReference(taskListWindow);
-
-        this.windows.add(progressBar);
-        this.windows.add(new FileExplorerWindow());
-        this.windows.add(taskWindow);
-        this.windows.add(new ReplayListWindow(taskListWindow, progressBar));
-        this.windows.add(taskListWindow);
-        this.windows.add(licensesWindow);*/
     }
 
     @Override
@@ -83,7 +62,6 @@ public class BobLite extends Application {
         if (ImGui.beginMenu("Menu")) {
             if (ImGui.menuItem("Licenses")) {
                 this.windowManager.getWindowByClass(LicensesWindow.class).setVisible(true);
-                //this.licensesWindow.setVisible(true);
             }
 
             ImGui.endMenu();
@@ -91,10 +69,8 @@ public class BobLite extends Application {
 
         ImGui.endMainMenuBar();
 
+        // we render all the windows
         this.windowManager.renderAll(menuBarHeight);
-
-        // and finally, we render the logger
-//        logger.render(menuBarHeight);
     }
 
     void start() {
