98b158db1d5b71e019d4f2e9924e52583f283689
Rader <raderpy@protonmail.com>
2022-11-01 00:20:08 +0100

    reworked licenses window

--- app/src/main/java/fr/rader/imbob/windows/impl/LicensesWindow.java
+++ app/src/main/java/fr/rader/imbob/windows/impl/LicensesWindow.java
@@ -1,12 +1,26 @@
 package fr.rader.imbob.windows.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.rader.imbob.utils.Pair;
 import fr.rader.imbob.windows.AbstractWindow;
 import imgui.ImGui;
 import imgui.flag.ImGuiWindowFlags;
 
 public class LicensesWindow extends AbstractWindow {
 
+    private final List<Pair<String, License>> dependencies;
+
+    private int columnSpacing;
+
     public LicensesWindow() {
+        this.dependencies = new ArrayList<>();
+        this.dependencies.add(new Pair<String, License>("gson", License.APACHE_2));
+        this.dependencies.add(new Pair<String, License>("imgui-java", License.APACHE_2));
+
+        this.columnSpacing = 100;
+
         setWindowFlags(ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoMove);
         setVisible(false);
         setWindowName("Licenses");
@@ -23,7 +37,46 @@ public class LicensesWindow extends AbstractWindow {
     protected void renderContent() {
         ImGui.text("This program uses the following open source software:");
         ImGui.newLine();
-        ImGui.text("Gson (Apache 2.0)");
-        ImGui.text("imgui-java (Apache 2.0)");
+
+        ImGui.columns(3);
+
+        if (this.columnSpacing != 0) {
+            ImGui.setColumnWidth(0, this.columnSpacing);
+            ImGui.setColumnWidth(1, this.columnSpacing);
+            this.columnSpacing = 0;
+        }
+
+        for (Pair<String, License> dependency : this.dependencies) {
+            ImGui.textUnformatted(dependency.getKey());
+
+            ImGui.nextColumn();
+            ImGui.textUnformatted(dependency.getValue().getName());
+
+            ImGui.nextColumn();
+            ImGui.textUnformatted(dependency.getValue().getLink());
+
+            ImGui.nextColumn();
+        }
+    }
+
+    private static enum License {
+
+        APACHE_2("Apache 2.0", "https://opensource.org/licenses/Apache-2.0");
+
+        private final String name;
+        private final String link;
+
+        private License(String name, String link) {
+            this.name = name;
+            this.link = link;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public String getLink() {
+            return this.link;
+        }
     }
 }
