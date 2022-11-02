4aa38208a3fce44132fdc1f731751c92e34beefa
Rader <raderpy@protonmail.com>
2022-11-02 12:27:19 +0100

    move to switch instead of ifs

--- app/src/main/java/fr/rader/imbob/tasks/impl/WeatherChangerTask.java
+++ app/src/main/java/fr/rader/imbob/tasks/impl/WeatherChangerTask.java
@@ -42,68 +42,72 @@ public class WeatherChangerTask extends AbstractTask {
     @Override
     public void execute(Packet packet, Queue<Packet> packets) {
         String newWeather = WEATHERS[this.selectedWeather.get()];
-        ProtocolVersion version = packet.getProtocolVersion();
+        ProtocolVersion protocolVersion = packet.getProtocolVersion();
 
-        if (packet.getPacketName().equals("join_game")) {
-            // we don't add any packet after the join game packet
-            // if we set the weather to clear in the replay
-            if (newWeather.equals("Clear")) {
-                return;
-            }
+        switch (packet.getPacketName()) {
+            case "join_game":
+                // we don't add any packet after the join game packet
+                // if we set the weather to clear in the replay
+                if (newWeather.equals("Clear")) {
+                    return;
+                }
 
-            // if we're here, this means the user wants to change the weather to either rain or thunder.
-            // as both require a rain level of 20, i first create a Change Game State packet to begin the rain,
-            // and then i create a second Change Game State packet to set the rain level to 1f
-            Packet beginRainPacket = new Packet(
-                    version,
-                    new VarInt(
-                            Packets.CHANGE_GAME_STATE.getPacketIdForProtocol(version)
-                    )
-            );
+                // if we're here, this means the user wants to change the weather to either rain or thunder.
+                // as both require a rain level of 20, i first create a Change Game State packet to begin the rain,
+                // and then i create a second Change Game State packet to set the rain level to 1f
+                Packet beginRainPacket = new Packet(
+                        protocolVersion,
+                        new VarInt(
+                                Packets.CHANGE_GAME_STATE.getPacketIdForProtocol(protocolVersion)
+                        )
+                );
 
-            // we add the Begin Rain reason, with a 0f value.
-            // the value doesn't matter, it can be anything
-            beginRainPacket.addEntry(new VariableEntry("reason", BEGIN_RAIN));
-            beginRainPacket.addEntry(new VariableEntry("value", 0f));
+                // we add the Begin Rain reason, with a 0f value.
+                // the value doesn't matter, it can be anything
+                beginRainPacket.addEntry(new VariableEntry("reason", BEGIN_RAIN));
+                beginRainPacket.addEntry(new VariableEntry("value", 0f));
 
-            // we clone an empty version of the beginRainPacket packet
-            Packet setRainLevelPacket = beginRainPacket.cloneEmpty();
-            // we add the Rain Level Change reason, with a 1f value.
-            // 1f means the rain is at it's full intensity.
-            setRainLevelPacket.addEntry(new VariableEntry("reason", RAIN_LEVEL_CHANGE));
-            setRainLevelPacket.addEntry(new VariableEntry("value", 1f));
+                // we clone an empty version of the beginRainPacket packet
+                Packet setRainLevelPacket = beginRainPacket.cloneEmpty();
+                // we add the Rain Level Change reason, with a 1f value.
+                // 1f means the rain is at it's full intensity.
+                setRainLevelPacket.addEntry(new VariableEntry("reason", RAIN_LEVEL_CHANGE));
+                setRainLevelPacket.addEntry(new VariableEntry("value", 1f));
 
-            // we add the packets to the queue with the correct order
-            packets.add(beginRainPacket);
-            packets.add(setRainLevelPacket);
+                // we add the packets to the queue with the correct order
+                packets.add(beginRainPacket);
+                packets.add(setRainLevelPacket);
 
-            // if the user wants to change the weather to thunder, we just create
-            // a third Change Game State packet and set the thunder level to 1f
-            if (newWeather.equals("Thunder")) {
-                Packet setThunderLevelPacket = beginRainPacket.cloneEmpty();
-                setThunderLevelPacket.addEntry(new VariableEntry("reason", THUNDER_LEVEL_CHANGE));
-                setThunderLevelPacket.addEntry(new VariableEntry("value", 1f));
+                // if the user wants to change the weather to thunder, we just create
+                // a third Change Game State packet and set the thunder level to 1f
+                if (newWeather.equals("Thunder")) {
+                    Packet setThunderLevelPacket = beginRainPacket.cloneEmpty();
+                    setThunderLevelPacket.addEntry(new VariableEntry("reason", THUNDER_LEVEL_CHANGE));
+                    setThunderLevelPacket.addEntry(new VariableEntry("value", 1f));
 
-                // we add the packet to the queue
-                packets.add(setThunderLevelPacket);
-            }
-        } else {
-            // we get the Change Game State packet's reason entry
-            int reason = packet.getEntry("reason").getAs(VariableEntry.class).getValueAs(Integer.class);
+                    // we add the packet to the queue
+                    packets.add(setThunderLevelPacket);
+                }
+                break;
 
-            // we return if the packet doesn't affect the weather
-            if (
-                    reason != BEGIN_RAIN &&
-                    reason != END_RAIN &&
-                    reason != RAIN_LEVEL_CHANGE &&
-                    reason != THUNDER_LEVEL_CHANGE
-            ) {
-                return;
-            }
+            case "change_game_state":
+                // we get the Change Game State packet's reason entry
+                int reason = packet.getEntry("reason").getAs(VariableEntry.class).getValueAs(Integer.class);
 
-            // otherwise, we just remove the packet from
-            // the queue so it doesn't get written
-            packets.poll();
+                // we return if the packet doesn't affect the weather
+                if (
+                        reason != BEGIN_RAIN &&
+                        reason != END_RAIN &&
+                        reason != RAIN_LEVEL_CHANGE &&
+                        reason != THUNDER_LEVEL_CHANGE
+                ) {
+                    return;
+                }
+
+                // otherwise, we just remove the packet from
+                // the queue so it doesn't get written
+                packets.poll();
+                break;
         }
     }
 
