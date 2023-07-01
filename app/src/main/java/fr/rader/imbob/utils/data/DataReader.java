a546a129223bbc907d7c1b2ef28dabcc6044e0de
Rader <raderpy@protonmail.com>
2023-07-01 21:01:52 +0200

    removed throws to read methods

--- app/src/main/java/fr/rader/imbob/utils/data/DataReader.java
+++ app/src/main/java/fr/rader/imbob/utils/data/DataReader.java
@@ -50,7 +50,7 @@ public class DataReader {
         this.inputStream = inputStream;
     }
 
-    public boolean readBoolean() throws IOException {
+    public boolean readBoolean() {
         int value = readByte();
 
         if (value <= 1) {
@@ -60,43 +60,49 @@ public class DataReader {
         throw new IllegalStateException("Boolean value above 1: " + value);
     }
 
-    public int readByte() throws IOException {
-        if (inputStream == null) {
+    public int readByte() {
+        if (this.inputStream == null) {
             throw new IllegalStateException("InputStream is null!");
         }
 
-        if (inputStream.available() == 0) {
-            throw new EOFException("Reached end of file!");
+        try {
+            if (this.inputStream.available() == 0) {
+                throw new EOFException("Reached end of file!");
+            }
+
+            return this.inputStream.read() & 0xFF;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        return inputStream.read() & 0xFF;
+        return 0;
     }
 
-    public int readShort() throws IOException {
+    public int readShort() {
         return (readByte() << 8 | readByte()) & 0xffff;
     }
 
-    public int readInt() throws IOException {
+    public int readInt() {
         return readShort() << 16 | readShort();
     }
 
-    public long readLong() throws IOException {
+    public long readLong() {
         return (long) readInt() << 32 | readInt() & 0xffffffffL;
     }
 
-    public float readFloat() throws IOException {
+    public float readFloat() {
         return ByteBuffer.wrap(readFollowingBytes(4)).order(ByteOrder.BIG_ENDIAN).getFloat();
     }
 
-    public double readDouble() throws IOException {
+    public double readDouble() {
         return ByteBuffer.wrap(readFollowingBytes(8)).order(ByteOrder.BIG_ENDIAN).getDouble();
     }
 
-    public char readChar() throws IOException {
+    public char readChar() {
         return (char) readByte();
     }
 
-    public byte[] readFollowingBytes(int length) throws IOException {
+    public byte[] readFollowingBytes(int length) {
         byte[] out = new byte[length];
 
         for (int i = 0; i < length; i++) {
@@ -106,7 +112,7 @@ public class DataReader {
         return out;
     }
 
-    public VarInt readVarInt() throws IOException {
+    public VarInt readVarInt() {
         int bytesRead = 0;
         int result = 0;
         int read;
@@ -125,7 +131,7 @@ public class DataReader {
         return new VarInt(result);
     }
 
-    public VarLong readVarLong() throws IOException {
+    public VarLong readVarLong() {
         int numRead = 0;
         long result = 0;
         int read;
@@ -144,7 +150,7 @@ public class DataReader {
         return new VarLong(result);
     }
 
-    public String readString(int length) throws IOException {
+    public String readString(int length) {
         StringBuilder out = new StringBuilder();
 
         while (length-- > 0) {
@@ -154,7 +160,7 @@ public class DataReader {
         return out.toString();
     }
 
-    public int[] readIntArray(int length) throws IOException {
+    public int[] readIntArray(int length) {
         int[] out = new int[length];
 
         for (int i = 0; i < length; i++) {
@@ -164,7 +170,7 @@ public class DataReader {
         return out;
     }
 
-    public long[] readLongArray(int length) throws IOException {
+    public long[] readLongArray(int length) {
         long[] out = new long[length];
 
         for (int i = 0; i < length; i++) {
@@ -174,7 +180,7 @@ public class DataReader {
         return out;
     }
 
-    public TagCompound readNBT() throws IOException {
+    public TagCompound readNBT() {
         byte firstByte = (byte) readByte();
         if (firstByte == 0) {
             return null;
@@ -183,12 +189,12 @@ public class DataReader {
         return new TagCompound(readString(readShort()), this);
     }
 
-    public UUID readUUID() throws IOException {
+    public UUID readUUID() {
         ByteBuffer byteBuffer = ByteBuffer.wrap(readFollowingBytes(16));
         return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
     }
 
-    public Position readPosition(Protocol protocolVersion) throws IOException {
+    public Position readPosition(Protocol protocolVersion) {
         long positionValue = readLong();
 
         int x;
@@ -208,7 +214,7 @@ public class DataReader {
         return new Position(protocolVersion, x, y, z);
     }
 
-    public Object readFromTokenType(TokenType type, Protocol protocolVersion) throws IOException {
+    public Object readFromTokenType(TokenType type, Protocol protocolVersion) {
         switch (type) {
             case BOOLEAN:
                 return readBoolean() ? 1 : 0;
@@ -278,7 +284,7 @@ public class DataReader {
 
     public int getLength() {
         try {
-            return inputStream.available();
+            return this.inputStream.available();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -288,19 +294,21 @@ public class DataReader {
 
     public void skip(int i) {
         try {
-            inputStream.skip(i);
+            this.inputStream.skip(i);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void close() {
-        if (inputStream != null) {
-            try {
-                inputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        if (this.inputStream == null) {
+            return;
+        }
+
+        try {
+            this.inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
